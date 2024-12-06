26a7b80e0886856afbd5b177b9707a95e209857f
LiamNeimi <liam.naeimi@tisipfagskole.no>
2024-12-06 16:41:40 +0100

    web-demo

---
+++ web-demo/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.12.0"
+    }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate07"
+    storage_account_name = "newsabetfstatevpmba2"
+    container_name       = "new-tfstate07"
+    key                  = "web-demo.terraform.tfstate-07"
+  }
+}
+provider "azurerm" {
+  features {}
+}
